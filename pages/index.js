25fefea53121e0e1eaf9cbd3b3d530a256a2c75d
Desinic8 <125421310+Desinic8@users.noreply.github.com>
2023-03-22 17:06:15 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Desinic8 - Providing Affordable Classroom Management Tools</title>
         <link rel="icon" href="/Favicon.png" />
       </Head>
 
